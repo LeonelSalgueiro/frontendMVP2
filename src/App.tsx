b7b73f534adaabef7b928a5181ac158e83881fa9
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-11-21 16:07:58 -0300

    acrescentando os arquivos de assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
-export function App(){
-  return <div></div>
-}
+export function Header() {
+  return (
+      <header>
+        <div>
+          <span>SecureCheck</span>
+        </div>
+
+        <div>
+          <h1>Sua identidade digital está segura?</h1>
+          <h3>Verifique instantaneamente se seu endereço de email foi exposto em vazamentos de dados conhecidos. Proteja-se agora.</h3>
+        </div>
+
+        <div>
+          <div>
+            <p>Criptografia de ponta a ponta</p>
+          </div>
+          <div>
+            <p>Verificação anônima</p>
+          </div>
+        </div>
+      </header>
+
+)}
